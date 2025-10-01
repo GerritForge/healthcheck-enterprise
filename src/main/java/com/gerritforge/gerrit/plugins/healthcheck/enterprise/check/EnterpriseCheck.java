e827dab422004a9b550ef3f9e4874a04030234ad
Luca Milanesio <luca.milanesio@gmail.com>
2025-10-01 14:31:47 +0100

    Initial empty skeleton with a fake EnterpriseCheck
    Change-Id: I98580657aa785575dcbfd5addb0b13f9f661e4f3

---
+++ src/main/java/com/gerritforge/gerrit/plugins/healthcheck/enterprise/check/EnterpriseCheck.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.healthcheck.enterprise.check;
+
+import static com.googlesource.gerrit.plugins.healthcheck.check.HealthCheckNames.GLOBAL;
+
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.gerrit.metrics.MetricMaker;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.healthcheck.HealthCheckConfig;
+import com.googlesource.gerrit.plugins.healthcheck.check.AbstractHealthCheck;
+
+@Singleton
+public class EnterpriseCheck extends AbstractHealthCheck {
+
+  @Inject
+  public EnterpriseCheck(
+      ListeningExecutorService executor, HealthCheckConfig config, MetricMaker metricMaker) {
+    super(executor, config, GLOBAL, metricMaker);
+  }
+
+  @Override
+  public Result doCheck() throws Exception {
+    return Result.PASSED;
+  }
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/healthcheck/check/EnterpriseCheckTest.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.healthcheck.check;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import com.gerritforge.gerrit.plugins.healthcheck.enterprise.check.EnterpriseCheck;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.testing.TestingExecutors;
+import com.google.gerrit.metrics.MetricMaker;
+import com.googlesource.gerrit.plugins.healthcheck.HealthCheckConfig;
+import org.junit.Test;
+
+public class EnterpriseCheckTest {
+
+  @Test
+  public void doCheck_returnsPassed() throws Exception {
+    ListeningExecutorService executor = TestingExecutors.sameThreadScheduledExecutor();
+    HealthCheckConfig config = mock(HealthCheckConfig.class);
+    MetricMaker metricMaker = mock(MetricMaker.class);
+
+    EnterpriseCheck check = new EnterpriseCheck(executor, config, metricMaker);
+
+    EnterpriseCheck.Result result = check.doCheck();
+
+    assertEquals(EnterpriseCheck.Result.PASSED, result);
+  }
+}
