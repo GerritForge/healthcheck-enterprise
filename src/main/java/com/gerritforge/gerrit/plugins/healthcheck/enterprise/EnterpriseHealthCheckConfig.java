e16079f961beaca6ef5c1126bfc2dd8d8e974f0f
Fabio Ponciroli <ponch78@gmail.com>
2025-10-10 15:41:58 +0200

    Add health check for JVM heap usage percentage
    Introduce a new health check named `jvm_heap_used` to monitor the
    percentage of JVM heap memory used by Gerrit. The check leverages the
    Java `MemoryMXBean` API to calculate heap usage and fails if it exceeds
    a configurable threshold, which defaults to 80%. The threshold value can
    be customized through the plugin configuration file.
    
    This enhancement helps proactively detect memory pressure on the JVM,
    improving observability and system reliability.
    
    Change-Id: I9b8d9461cde6f1aa017d6612008c53b0c76b04f2

---
+++ src/main/java/com/gerritforge/gerrit/plugins/healthcheck/enterprise/EnterpriseHealthCheckConfig.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.healthcheck.enterprise;
+
+import com.google.gerrit.server.config.AllProjectsName;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.config.GerritIsReplica;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.healthcheck.HealthCheckConfig;
+import org.eclipse.jgit.lib.Config;
+
+@Singleton
+public class EnterpriseHealthCheckConfig extends HealthCheckConfig {
+  private static final int JVM_MEMORY_PERCENTAGE_DEFAULT = 80;
+
+  private final Config config;
+
+  @Inject
+  public EnterpriseHealthCheckConfig(
+      PluginConfigFactory configFactory,
+      AllProjectsName allProjectsName,
+      AllUsersName allUsersName,
+      @GerritIsReplica boolean isReplica) {
+    super(configFactory, allProjectsName, allUsersName, isReplica);
+    config = configFactory.getGlobalPluginConfig(HEALTHCHECK_PLUGIN_NAME);
+  }
+
+  public int getJVMHeapPercentageThreshold(String healthCheckName) {
+    int defaultThreshold =
+        healthCheckName == null
+            ? JVM_MEMORY_PERCENTAGE_DEFAULT
+            : getJVMHeapPercentageThreshold(null);
+    return config.getInt(HEALTHCHECK, healthCheckName, "threshold", defaultThreshold);
+  }
+}
---
+++ src/main/java/com/gerritforge/gerrit/plugins/healthcheck/enterprise/check/JVMHeapCheck.java
@@ -0,0 +1,80 @@
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.healthcheck.enterprise.check;
+
+import static com.gerritforge.gerrit.plugins.healthcheck.enterprise.check.HealthCheckNames.JVM_HEAP_USED;
+
+import com.gerritforge.gerrit.plugins.healthcheck.enterprise.EnterpriseHealthCheckConfig;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.flogger.FluentLogger;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.gerrit.metrics.MetricMaker;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.healthcheck.check.AbstractHealthCheck;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+
+@Singleton
+public class JVMHeapCheck extends AbstractHealthCheck {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final Integer threshold;
+  private final MemoryMXBean memoryMXBean;
+
+  @Inject
+  public JVMHeapCheck(
+      ListeningExecutorService executor,
+      EnterpriseHealthCheckConfig config,
+      MetricMaker metricMaker) {
+    super(executor, config, JVM_HEAP_USED, metricMaker);
+    this.threshold = config.getJVMHeapPercentageThreshold(JVM_HEAP_USED);
+    this.memoryMXBean = ManagementFactory.getMemoryMXBean();
+  }
+
+  @VisibleForTesting
+  public JVMHeapCheck(
+      ListeningExecutorService executor,
+      EnterpriseHealthCheckConfig config,
+      MetricMaker metricMaker,
+      MemoryMXBean memoryMXBean) {
+    super(executor, config, JVM_HEAP_USED, metricMaker);
+    this.threshold = config.getJVMHeapPercentageThreshold(JVM_HEAP_USED);
+    this.memoryMXBean = memoryMXBean;
+  }
+
+  @Override
+  public Result doCheck() throws Exception {
+    MemoryUsage heapUsage = memoryMXBean.getHeapMemoryUsage();
+
+    long max = heapUsage.getMax();
+
+    // getMax() may be -1 if undefined, however it should never happen in Gerrit since Xmx is always set
+    if (max <= 0) {
+      logger.atWarning().log(
+          "Max memory is undefined. Check 'container.heapLimit' in you configuration");
+      return Result.PASSED;
+    }
+
+    long used = heapUsage.getUsed();
+    double pct = (double) used * 100.0 / max;
+
+    logger.atSevere().log(
+        "Max heap allocated: %d, Heap used %d, Percentage used: %.2f, threshold: %d",
+        max, used, pct, threshold);
+    if (pct > threshold) {
+      return Result.FAILED;
+    }
+    return Result.PASSED;
+  }
+}
