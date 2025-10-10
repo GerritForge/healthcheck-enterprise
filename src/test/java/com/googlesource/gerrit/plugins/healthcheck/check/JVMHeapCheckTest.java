217c4d124be0955a1daca3d057c37019b6eea198
Fabio Ponciroli <ponch78@gmail.com>
2025-10-10 15:41:58 +0200

    Add health check for JVM heap usage percentage
    Introduce a new health check named `jvm_heap_used` to monitor the
    percentage of JVM heap memory used by Gerrit. The check leverages the
    Java `MemoryMXBean` API to calculate heap usage and fails if it exceeds
    a configurable threshold, which defaults to 80%. The threshold value can
    be customized through the plugin configuration file.
    
    This enhancement helps proactively detect memory pressure on the JVM,
    improving observability and system reliability.
    
    Change-Id: I9b8d9461cde6f1aa017d6612008c53b0c76b04f2

---
+++ src/test/java/com/googlesource/gerrit/plugins/healthcheck/check/JVMHeapCheckTest.java
@@ -0,0 +1,128 @@
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.healthcheck.check;
+
+import static com.googlesource.gerrit.plugins.healthcheck.TestUtils.enterpriseHealthCheckConfig;
+import static com.googlesource.gerrit.plugins.healthcheck.TestUtils.getConfigWithThreshold;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.gerritforge.gerrit.plugins.healthcheck.enterprise.EnterpriseHealthCheckConfig;
+import com.gerritforge.gerrit.plugins.healthcheck.enterprise.check.JVMHeapCheck;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.testing.TestingExecutors;
+import com.google.gerrit.metrics.MetricMaker;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+import org.eclipse.jgit.lib.Config;
+import org.junit.Test;
+
+public class JVMHeapCheckTest {
+
+  @Test
+  public void doCheck_returnsPassed() throws Exception {
+    ListeningExecutorService executor = TestingExecutors.sameThreadScheduledExecutor();
+
+    EnterpriseHealthCheckConfig enterpriseHealthCheckConfig =
+        enterpriseHealthCheckConfig(new Config());
+
+    TestMocks mocks = getMocks();
+
+    JVMHeapCheck check =
+        new JVMHeapCheck(
+            executor, enterpriseHealthCheckConfig, mocks.metricMaker(), mocks.memoryMXBean());
+
+    when(mocks.memoryUsage().getMax()).thenReturn(100L);
+    when(mocks.memoryUsage().getUsed()).thenReturn(1L);
+
+    HealthCheck.Result result = check.doCheck();
+
+    assertEquals(HealthCheck.Result.PASSED, result);
+  }
+
+  @Test
+  public void doCheck_returnsPassWhenMaxUsageUndefined() throws Exception {
+    ListeningExecutorService executor = TestingExecutors.sameThreadScheduledExecutor();
+    EnterpriseHealthCheckConfig enterpriseHealthCheckConfig =
+        enterpriseHealthCheckConfig(new Config());
+
+    TestMocks mocks = getMocks();
+
+    JVMHeapCheck check =
+        new JVMHeapCheck(
+            executor, enterpriseHealthCheckConfig, mocks.metricMaker(), mocks.memoryMXBean());
+
+    when(mocks.memoryUsage().getMax()).thenReturn(-1L);
+    when(mocks.memoryUsage().getUsed()).thenReturn(90L);
+
+    HealthCheck.Result result = check.doCheck();
+
+    assertEquals(HealthCheck.Result.PASSED, result);
+  }
+
+  @Test
+  public void doCheck_returnsFailWithDefaultThreshold() throws Exception {
+    ListeningExecutorService executor = TestingExecutors.sameThreadScheduledExecutor();
+    EnterpriseHealthCheckConfig enterpriseHealthCheckConfig =
+        enterpriseHealthCheckConfig(new Config());
+    MetricMaker metricMaker = mock(MetricMaker.class);
+    MemoryMXBean memoryMXBean = mock(MemoryMXBean.class);
+    MemoryUsage memoryUsage = mock(MemoryUsage.class);
+    when(memoryMXBean.getHeapMemoryUsage()).thenReturn(memoryUsage);
+
+    JVMHeapCheck check =
+        new JVMHeapCheck(executor, enterpriseHealthCheckConfig, metricMaker, memoryMXBean);
+
+    when(memoryUsage.getMax()).thenReturn(100L);
+    when(memoryUsage.getUsed()).thenReturn(90L);
+
+    HealthCheck.Result result = check.doCheck();
+
+    assertEquals(HealthCheck.Result.FAILED, result);
+  }
+
+  @Test
+  public void doCheck_returnsFailWithCustomThreshold() throws Exception {
+    ListeningExecutorService executor = TestingExecutors.sameThreadScheduledExecutor();
+
+    int customThreshold = 50;
+    MetricMaker metricMaker = mock(MetricMaker.class);
+    EnterpriseHealthCheckConfig enterpriseHealthCheckConfig =
+        enterpriseHealthCheckConfig(getConfigWithThreshold(customThreshold));
+
+    MemoryMXBean memoryMXBean = mock(MemoryMXBean.class);
+    MemoryUsage memoryUsage = mock(MemoryUsage.class);
+    when(memoryMXBean.getHeapMemoryUsage()).thenReturn(memoryUsage);
+
+    JVMHeapCheck check =
+        new JVMHeapCheck(executor, enterpriseHealthCheckConfig, metricMaker, memoryMXBean);
+
+    when(memoryUsage.getMax()).thenReturn(100L);
+    when(memoryUsage.getUsed()).thenReturn((long) (customThreshold + 1));
+
+    HealthCheck.Result result = check.doCheck();
+
+    assertEquals(HealthCheck.Result.FAILED, result);
+  }
+
+  private static TestMocks getMocks() {
+    MetricMaker metricMaker = mock(MetricMaker.class);
+    MemoryMXBean memoryMXBean = mock(MemoryMXBean.class);
+    MemoryUsage memoryUsage = mock(MemoryUsage.class);
+    when(memoryMXBean.getHeapMemoryUsage()).thenReturn(memoryUsage);
+    return new TestMocks(metricMaker, memoryMXBean, memoryUsage);
+  }
+
+  private record TestMocks(
+      MetricMaker metricMaker, MemoryMXBean memoryMXBean, MemoryUsage memoryUsage) {}
+}
