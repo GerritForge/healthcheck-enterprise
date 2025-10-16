7ed0694d81abd892cbfafcd53391e0f232a71f6b
Fabio Ponciroli <ponch78@gmail.com>
2025-10-16 10:13:23 +0200

    Move tests under correct package name
    Change-Id: I3b65be1bce8b2ca74b8c8bd4f5f40fd122db0aa2

--- src/test/java/com/gerritforge/gerrit/plugins/healthcheck/enterprise/check/EnterpriseCheckTest.java
+++ src/test/java/com/gerritforge/gerrit/plugins/healthcheck/enterprise/check/EnterpriseCheckTest.java
@@ -9,7 +9,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.googlesource.gerrit.plugins.healthcheck.check;
+package com.gerritforge.gerrit.plugins.healthcheck.enterprise.check;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
